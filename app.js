ebc6a33180ea97dd2a535cd8b35de3a71d907621
Reizy Raven <rayzafir@aol.com>
2021-09-19 17:06:22 -0400

    removing unused code

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
 let diceAmount = 4;
 let counter = 0;
+let values = []
 
 for (let index = 0; index <= diceAmount; index++) {
   let diceName = "dice" + index.toString();
-  console.log(diceName);
-  $( '.dice' ).append("<a onclick = 'diceSelect('diceName')'><div id = '" + diceName + "'><ol class='die-list even-roll' data-roll='1' id='die-2'> <li class='die-item' data-side='1' id = 'side-1'></li> <li class='die-item' data-side='2'id = 'side-2'></li><li class='die-item' data-side='3' id = 'side-3'></li><li class='die-item' data-side='4' id = 'side-4'></li> <li class='die-item' data-side='5' id = 'side-5'></li><li class='die-item' data-side='6' id = 'side-6'></li></ol></div></a>");
+  $( '.dice' ).append("<a onclick = 'diceSelect("+ diceName +")'><div id = '" + diceName + "'><ol class='die-list even-roll' data-roll='1' id='die-2'> <li class='die-item' data-side='1' id = 'side-1'></li> <li class='die-item' data-side='2'id = 'side-2'></li><li class='die-item' data-side='3' id = 'side-3'></li><li class='die-item' data-side='4' id = 'side-4'></li> <li class='die-item' data-side='5' id = 'side-5'></li><li class='die-item' data-side='6' id = 'side-6'></li></ol></div></a>");
 } 
 
-while (counter >= 2) {
-  document.getElementById('roll-button').disabled = true;
-  $('#button-group').append('<button type="button" id="restartButton" onclick="addDice("restart")">Restart</button>');
-}
-
-function diceSelect(diceId){
-  if(document.getElementById(diceId).firstChild.classList.contains("selected")){
-    document.getElementById(diceId).firstChild.classList.remove("selected");
+  function diceSelect(diceId){
+    console.log(diceId.id);
+    let dice = document.getElementById(diceId.id);
+    if(dice.classList.contains('selected')){
+      dice.classList.remove('selected');
+    }
+    else{
+      dice.classList.add('selected');
+    }
   }
-  else{
-    document.getElementById(diceId).firstChild.classList.add("selected");
-  }
-}
-
-function rollDice() {
-    const dice = [...document.querySelectorAll('.die-list')];
-    dice.forEach(die => {
-      if(die.classList.contains("selected")){
-        return;
+  function rollDice() {
+      const dice = [...document.querySelectorAll('.die-list')];
+      dice.forEach(die => {
+        if(die.parentElement.classList.contains('selected')){
+          console.log('ive been selected!')
+          return;
+        }
+        else{
+          toggleClasses(die);
+          die.dataset.roll = getRandomNumber(1, 6);
+        }
+      });
+    console.log("You have " + (3-(counter +1)) + " spins left!");   
+      if(counter >= 2) {
+        document.getElementById('roll-button').style.display = 'none';
+        document.getElementById('plusOne').style.display = 'none';
+        document.getElementById('plusTwo').style.display = 'none';
+        document.getElementById('restart-button').style.display = 'inline-block';
       }
-      else{
-        toggleClasses(die);
-        die.dataset.roll = getRandomNumber(1, 6);
-      }
-    });
-  console.log("You have " + (3-(counter +1)) + " spins left!");  
-  return counter ++;  
-  }
-  
+    return counter++;
+    }
+  function restart(){
+    document.getElementById('roll-button').style.display = 'inline-block';
+    document.getElementById('plusOne').style.display = 'inline-block';
+    document.getElementById('plusTwo').style.display = 'inline-block';
+    document.getElementById('restart-button').style.display = 'none';
+  }  
   function toggleClasses(die) {
     die.classList.toggle('odd-roll');
     die.classList.toggle('even-roll');
@@ -50,7 +58,7 @@ function rollDice() {
   function addDice(toggleDice) {
     let diceName = "dice";
     if(diceAmount == 4){
-      $( '.dice' ).append("<a onclick = 'diceSelect('diceName')'><div id = '" + diceName + "'><ol class='die-list even-roll' data-roll='1' id='die-2'> <li class='die-item' data-side='1' id = 'ex-side-1'></li> <li class='die-item' data-side='2'id = 'ex-side-2'></li><li class='die-item' data-side='3' id = 'ex-side-3'></li><li class='die-item' data-side='4' id = 'ex-side-4'></li> <li class='die-item' data-side='5' id = 'ex-side-5'></li><li class='die-item' data-side='6' id = 'ex-side-6'></li></ol></div></a>");
+      $( '.dice' ).append("<a onclick = 'diceSelect("+ diceName +")'><div id = '" + diceName + "'><ol class='die-list even-roll' data-roll='1' id='die-2'> <li class='die-item' data-side='1' id = 'ex-side-1'></li> <li class='die-item' data-side='2'id = 'ex-side-2'></li><li class='die-item' data-side='3' id = 'ex-side-3'></li><li class='die-item' data-side='4' id = 'ex-side-4'></li> <li class='die-item' data-side='5' id = 'ex-side-5'></li><li class='die-item' data-side='6' id = 'ex-side-6'></li></ol></div></a>");
       diceAmount++;
       //displayDice = false;
       document.getElementById(toggleDice).style.backgroundColor = '#4de34d';
@@ -65,7 +73,7 @@ function rollDice() {
   function addSecondDice(toggleDice) {
     let diceName = "dice";
     if(diceAmount == 5){
-      $( '.dice' ).append("<a onclick = 'diceSelect('diceName')'><div id = '" + diceName + "'><ol class='die-list even-roll' data-roll='1' id='die-2'> <li class='die-item' data-side='1' id = 'ex-side-1'></li> <li class='die-item' data-side='2' id = 'ex-side-2'></li><li class='die-item' data-side='3' id = 'ex-side-3'></li><li class='die-item' data-side='4' id = 'ex-side-4'></li> <li class='die-item' data-side='5' id = 'ex-side-5'></li><li class='die-item' data-side='6' id = 'ex-side-6'></li></ol></div></a>");
+      $( '.dice' ).append("<a onclick = 'diceSelect("+ diceName +")'><div id = '" + diceName + "'><ol class='die-list even-roll' data-roll='1' id='die-2'> <li class='die-item' data-side='1' id = 'ex-side-1'></li> <li class='die-item' data-side='2'id = 'ex-side-2'></li><li class='die-item' data-side='3' id = 'ex-side-3'></li><li class='die-item' data-side='4' id = 'ex-side-4'></li> <li class='die-item' data-side='5' id = 'ex-side-5'></li><li class='die-item' data-side='6' id = 'ex-side-6'></li></ol></div></a>");
       diceAmount++;
       //displayDice = false;
       document.getElementById(toggleDice).style.backgroundColor = '#4de34d';
